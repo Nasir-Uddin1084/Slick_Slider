00df75506eb9d860181ba17a53234d8617219898
Nasir Uddin <uddin6058@gmail.com>
2021-08-06 17:40:06 +0600

    second commit

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		fade: false,
-		speed: 2000,
+		speed: 1000,
 		autoplay: true,
 		// if use fade, do not use autoplaySpeed.
 		// autoplaySpeed: 2000,
 		cssEase: true,
-    dots: false,
-    arrows: false,
+		dots: false,
+		arrows: false
 	});
 });
