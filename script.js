5957374fe10845ff96a17f007e2570a4e3315a80
Nasir Uddin <uddin6058@gmail.com>
2021-08-08 15:57:32 +0600

    slider part 2

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-$(document).ready(function() {
-	$('.slider').slick({
+$(document).ready(function () {
+	// slider1
+	$('.slider1').slick({
 		infinite: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		fade: false,
-		speed: 1000,
+		speed: 1500,
 		autoplay: true,
 		// if use fade, do not use autoplaySpeed.
 		// autoplaySpeed: 2000,
 		cssEase: true,
 		dots: false,
-		arrows: false
+		arrows: false,
+	});
+	// slider2
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		dots: false,
+		cssEase: false,
+		arrows: false,
 	});
 });
